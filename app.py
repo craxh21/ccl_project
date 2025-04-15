a4e319e3a057e1a4c3bc06408ec19fa4de492ece
unknown <mbcraxh21@gmail.com>
2025-04-16 02:19:42 +0530

    port

--- app.py
+++ app.py
@@ -46,4 +46,5 @@ def upload_file():
     return jsonify({"status": "success", "message": "File uploaded"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
